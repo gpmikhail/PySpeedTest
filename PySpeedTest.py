bdce89da722d9889fbdad802bb2aaef0021435f7
gpmikhail <g.p.mikhail@live.com>
2022-02-27 03:40:51 +0300

    Added file logging
    Added file logging.
    Results are saved in speedtest.txt in the same folder where the script runs.

--- PySpeedTest.py
+++ PySpeedTest.py
@@ -1,15 +1,24 @@
 import speedtest
 from datetime import datetime
 
+f = open("speedtest.txt","a")
+
 download_speed = speedtest.Speedtest().download()
 dspd = round(download_speed / 2 ** 20, 2)
-print('DownloadSpd = ', dspd, ' Mbps')
+dl_out = 'DownloadSpd = ' + str(dspd) + ' Mbps'
+print(dl_out)
+f.write(dl_out + '\n') 
 
 upload_speed = speedtest.Speedtest().upload()
 uspd = round(upload_speed / 2 ** 20, 2) 
-print('UploadSpd   = ', uspd, ' Mbps')
+ul_out = 'UploadSpd   = ' + str(uspd) + ' Mbps' 
+print(ul_out)
+f.write(ul_out + '\n')
 
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 print(dt_string)
+f.write(dt_string + '\n\n')
+
+f.close()
